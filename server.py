3f29ac429b6eb16e64f2656b59da4b2913d2a3bb
Amanda Nguyen <amanda6@ualberta.ca>
2022-09-28 19:29:03 -0600

    completed assignment 1

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #  coding: utf-8 
+from re import I
 import socketserver
-
+import os 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,15 +25,76 @@ import socketserver
 #
 # run: python freetests.py
 
+# Copyright 2022 Amanda Nguyen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+def readfile(folder_path):
+    f = open(folder_path, 'r')
+    data= ''
+    for line in f:
+        data = data + line
+    return data
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        ## decode data
+        decode_data = self.data.decode("utf-8").split("\r\n")
+        method, path, throwaway = decode_data[0].split(" ")
+
+        ## if method is NOT GET return 405
+        if "GET" != method:
+            self.request.sendall(bytearray(
+                        "HTTP/1.1 405 METHOD NOT ALLOWED\r\n", 'utf-8'))
+        else:
+            ## if it is not .html or .css AND it doesn't end with / then redirect
+            if "." not in path and "/" != path[-1]:
+                self.request.sendall(bytearray(
+                            f"HTTP/1.1 301 MOVED PERMANENTLY\r\nLocation: {path}/\r\n", 'utf-8'))
+                            
+            ## if its specifically 
+            if "." not in path:
+                path += "index.html"
+
+            if "." in path and path[-1] == "/": #for some reason base.css gets turned into base.css/
+                path = path[:-1]
+
+            folder_path = os.getcwd() + "/www" + path
+
+            if ".html" in path or ".css" in path: # resolves the safety test
+                try: # exception handles if we get a path that doesn't exist in a folder
+                    if ".html" in path:
+                        print("jtm,l")
+                        data = readfile(folder_path)
+                        self.request.sendall(bytearray(f"HTTP/1.1 200 OK\r\ncontent-type: text/html\r\nConnection: close\r\n\r\n\r\n{data}" , 'utf-8'))
+
+                    elif ".css" in path:
+                        print("html")
+                        data = readfile(folder_path)
+                        self.request.sendall(bytearray(f"HTTP/1.1 200 OK\r\ncontent-type: text/css\r\nConnection: close\r\n\r\n\r\n{data}" , 'utf-8'))
+                except:
+                    print("hio")
+                    self.request.sendall(bytearray(
+                                "HTTP/1.1 404 NOT FOUND\r\nConnection: close\r\n\r\n\r\n", 'utf-8'))
+            else: 
+                self.request.sendall(bytearray(
+                                "HTTP/1.1 404 NOT FOUND\r\nConnection: close\r\n\r\n\r\n", 'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
